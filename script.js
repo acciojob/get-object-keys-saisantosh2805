d1c3a0e85bf4861d2c41c6bf33d1275ba9bbf24f
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-11-05 01:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Create the student object with a name property
+const student = {
+  name: "John Doe",
+};
